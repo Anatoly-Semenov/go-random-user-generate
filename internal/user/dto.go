d349b45abbd3a419068ad3bd2ff203447179b568
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-08-18 14:37:43 +0300

    Create user dto

--- internal/user/dto.go
+++ internal/user/dto.go
@@ -1,6 +1,12 @@
 package user
 
 type CreateUserDto struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Dob       string `json:"dob"`
+	Age       int    `json:"age"`
+	Sex       Sex    `json:"sex"`
+	Avatar    string `json:"avatar"`
 }
 
 type UpdateUserDto struct {
