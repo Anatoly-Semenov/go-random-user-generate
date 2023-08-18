59f5b9ee670779424273486f7437cc079bdf8b74
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-08-18 14:36:17 +0300

    Create config with default users

---
+++ internal/user/config.go
@@ -0,0 +1,42 @@
+package user
+
+var (
+	Users = []*Model{
+		{
+			Id:        1,
+			FirstName: "Cathy",
+			LastName:  "Garrett",
+			Dob:       "04.07.1984",
+			Age:       33,
+			Sex:       "female",
+			Avatar:    "https://randomuser.me/api/portraits/women/51.jpg",
+		},
+		{
+			Id:        2,
+			FirstName: "Amber",
+			LastName:  "Carlson",
+			Dob:       "12.12.1990",
+			Age:       33,
+			Sex:       "female",
+			Avatar:    "https://randomuser.me/api/portraits/women/31.jpg",
+		},
+		{
+			Id:        3,
+			FirstName: "Jackie",
+			LastName:  "Welch",
+			Dob:       "05.03.1997",
+			Age:       33,
+			Sex:       "male",
+			Avatar:    "https://randomuser.me/api/portraits/men/30.jpg",
+		},
+		{
+			Id:        4,
+			FirstName: "Donald",
+			LastName:  "Welch",
+			Dob:       "12.12.1990",
+			Age:       33,
+			Sex:       "male",
+			Avatar:    "https://randomuser.me/api/portraits/men/5.jpg",
+		},
+	}
+)
